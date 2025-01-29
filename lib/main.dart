7698fc4eba7c55b23ffc4686a55c9be6fb40f0a7
Alexey Kudryashov <akd@krononsoft.com>
2025-01-29 16:56:57 +0300

    Fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:expence_tracker/expenses.dart';
+import 'package:expenses_tracker/expenses.dart';
 
 void main() {
   runApp(const MaterialApp(
